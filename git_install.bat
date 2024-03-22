5482bad411f538252f8f8e4a0d6d90260dd59bdc
Jeong-Hyun Kim <jeong-hyun.kim@student.manchester.ac.uk>
2024-03-22 03:06:53 +0100

    [BATCH] Commit changes via batch script

---
+++ git_install.bat
@@ -0,0 +1,7 @@
+:: echo "# EEEN10202" >> README.md
+git init
+:: git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Ashley5478/EEEN10202.git
+git push -u origin main
---
+++ git_push.bat
@@ -0,0 +1,6 @@
+git remote add origin https://github.com/Ashley5478/EEEN10202.git
+git branch -M main
+git add -A
+git add -A
+git commit -m "[BATCH] Commit changes via batch script"
+git push -u origin main
